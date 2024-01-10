7c3953a3f3142f7897c5630707c0b5b57ce817d7
Anabella <acastle40@stuy.edu>
2024-01-10 14:39:02 -0500

    they connect

--- player.c
+++ player.c
@@ -1,8 +1,36 @@
-#include "connections.h"
+#include "rps.h"
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void err(int i, char*message){
+  if(i < 0){
+	  printf("Error: %s - %s\n",message, strerror(errno));
+  	exit(1);
+  }
+}
+
+int client_connection(){
+  struct addrinfo *hints, *results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  getaddrinfo("127.0.0.1", "4242", hints, &results);
+  int connector = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+  err(connector,"connector socket setup");
+  connect(connector, results->ai_addr, results->ai_addrlen);
+  printf("connected\n");
+  free(hints);
+  freeaddrinfo(results);
+  return connector;
+}
 
 int main(){
+  printf("player\n");
   client_connection();
   return 0;
 }
--- server.c
+++ server.c
@@ -32,13 +32,40 @@ int server_setup(){ //returns socket descriptor
   int binder = bind(listener, results->ai_addr, results->ai_addrlen);
   err(binder,"binding socket to address and port");
   //inlistening state
-  listen(listener, 10);
-  printf("ready to listen\n");
   return listener;
 }
 
 int main(){
-  printf("ah");
-  server_setup();
+  printf("Setup time\n");
+  struct addrinfo *hints, *results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  char *port = "4242";
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM; //TCP socke
+  hints->ai_flags = AI_PASSIVE; //only needed on server
+  getaddrinfo("127.0.0.1", port, hints, &results);
+  //main socket creation
+  int listener = socket(results->ai_family, results->ai_socktype, results->ai_protocol);//store the socket descriptor here
+  err(listener,"make socket");
+  //this code should get around the address in use error
+  int yes = 1;
+  int sockOpt =  setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  err(sockOpt,"sockopt error");
+  //bind socket
+  int binder = bind(listener, results->ai_addr, results->ai_addrlen);
+  err(binder,"binding socket to address and port");
+
+  listen(listener, 10);
+  printf("ready to listen\n");
+
+  socklen_t sock_size;
+  struct sockaddr_storage client_address;
+  sock_size = sizeof(client_address);
+  int client_socket = accept(listener,(struct sockaddr *)&client_address, &sock_size);
+  err(client_socket,"acceptor failure");
+
+  free(hints);
+  freeaddrinfo(results);
+
   return 0;
 }
