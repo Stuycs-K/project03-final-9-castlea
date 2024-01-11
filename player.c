8c3d334cf7e3410b03d1a820f6f5b5fd087f8d84
Anabella <acastle40@stuy.edu>
2024-01-11 14:50:16 -0500

    working on storing the players and having a tournament vs not tournament mode

--- player.c
+++ player.c
@@ -7,6 +7,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//int send(int sockfd, const void *msg, int len, int flags);
+//sockfd is the socket descriptor you want to send data to (whether it’s the one returned by socket() or the one you got with accept()).
+// is a pointer to the data you want to send, and len is the length of that data in bytes.
+//set flags to 0
+
+//int recv(int sockfd, void *buf, int len, int flags);
+//sockfd is the socket descriptor to read from
+// is the buffer to read the information into
+// is the maximum length of the buffer, and flags can again be set to 0
+
+//int getpeername(int sockfd, struct sockaddr *addr, int *addrlen);
+//sockfd is the descriptor of the connected stream socket
+//addr is a pointer to a struct sockaddr (or a struct sockaddr_in) that will hold the information about the other side of the connection,
+//addrlen is a pointer to an int, that should be initialized to sizeof *addr or sizeof(struct sockaddr).
+
+
+
 void err(int i, char*message){
   if(i < 0){
 	  printf("Error: %s - %s\n",message, strerror(errno));
@@ -31,6 +48,14 @@ int client_connection(){
 
 int main(){
   printf("player\n");
-  client_connection();
+  int accepted = client_connection();
+  char buff[200];
+  int rere = read(accepted,buff,sizeof(buff));
+  err(rere,"recieving first message");
+  printf("%s\n",buff);
+  fgets(buff,sizeof(buff),stdin);
+  int hereName = send(accepted,buff,sizeof(buff),0);
+
+
   return 0;
 }
--- server.c
+++ server.c
@@ -5,34 +5,13 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/select.h>
 
 void err(int i, char*message){
   if(i < 0){
-	  printf("Error: %s - %s\n",message, strerror(errno));
-  	exit(1);
+    printf("Error: %s - %s\n",message, strerror(errno));
+    exit(1);
   }
-}
-
-int server_setup(){ //returns socket descriptor
-  //getaddrinfo
-  struct addrinfo *hints, *results;
-  char *port = "4242";
-  hints->ai_family = AF_INET;
-  hints->ai_socktype = SOCK_STREAM; //TCP socke
-  hints->ai_flags = AI_PASSIVE; //only needed on server
-  getaddrinfo("127.0.0.1", port, hints, &results);
-  //main socket creation
-  int listener = socket(results->ai_family, results->ai_socktype, results->ai_protocol);//store the socket descriptor here
-  err(listener,"make socket in server_setup");
-  //this code should get around the address in use error
-  int yes = 1;
-  int sockOpt =  setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-  err(sockOpt,"sockopt  error");
-  //bind socket
-  int binder = bind(listener, results->ai_addr, results->ai_addrlen);
-  err(binder,"binding socket to address and port");
-  //inlistening state
-  return listener;
 }
 
 int main(){
@@ -61,8 +40,57 @@ int main(){
   socklen_t sock_size;
   struct sockaddr_storage client_address;
   sock_size = sizeof(client_address);
-  int client_socket = accept(listener,(struct sockaddr *)&client_address, &sock_size);
-  err(client_socket,"acceptor failure");
+  /*int client_socket = accept(listener,(struct sockaddr *)&client_address, &sock_size);
+  err(client_socket,"acceptor failure");*/
+
+  fd_set read_fds;
+  char buff[1025]="";
+  int lineup[10]; //stores player socket codes
+  int i = 0;
+  int tournament = 0; //variable for when game is in session
+  while(1){
+
+    FD_ZERO(&read_fds);
+    FD_SET(listener,&read_fds);
+    int x = select(listener+1, &read_fds, NULL, NULL, NULL);
+    err(x,"select");
+
+    // if socket
+    if (FD_ISSET(listener, &read_fds)) {
+      //accept the connection
+      if(!tournament){
+        int client_socket = accept(listener,(struct sockaddr *)&client_address, &sock_size);
+        err(client_socket,"acceptor failure");
+        lineup[i]=client_socket; //store client
+        char wutName[200];
+        sprintf(wutName,"Hello, you are Player %d. Are we waiting on more players? (use lowercase)",i+1);
+        int gimmeName = send(client_socket, wutName, strlen(wutName)+1, 0);
+        err(gimmeName,"initial message");
+
+        //read the whole buff
+        int ro = read(client_socket,buff,sizeof(buff));
+        err(ro,"recieving response to more players question");
+        printf("Player %d confirmed\n",i+1);
+
+        //trim the string
+        buff[strlen(buff)-1]=0; //clear newline
+        if(buff[strlen(buff)-1]==13){
+          //clear windows line ending
+          buff[strlen(buff)-1]=0;
+        }
+        if(strcmp(buff,"no")){
+          tournament = 1;
+        }
+
+        //printf("\nPlayer Name: '%s'\n",buff);
+        i++;
+      }
+      if(tournament){
+
+      }
+      //close(client_socket);
+    }
+  }
 
   free(hints);
   freeaddrinfo(results);
